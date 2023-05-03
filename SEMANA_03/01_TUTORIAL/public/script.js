9f4e54d30b738e59cbee41b97cd43ac94e6274a8
Kaiane <kaiane.cordeiro@sou.inteli.edu.br>
2023-05-03 09:36:59 -0300

    etapa03

--- SEMANA_03/01_TUTORIAL/public/script.js
+++ SEMANA_03/01_TUTORIAL/public/script.js
@@ -1,11 +1,22 @@
 ﻿/* Função que calcula uma soma e mostra a operação realizada e o resultado obtido */
-function calcula(x, y){
-    var op = "+";
+function calcula(x, y, op = "+"){
     var resultado;
-    if (op == "+") {
+
+    if (op === "+") {
         resultado = x + y;
-    };
+    }
+
+    if (op === "-") {
+        resultado = x - y;
+    }
+
+    if(op === "*") {
+        resultado = x * y; 
+    }
+
+    if (op === "/") {
+        resultado = x / y
+    }
     document.getElementById("saida").innerHTML = `${x} ${op} ${y} = ${resultado}`;
     console.log(`<br /> ${x} ${op} ${y} = ${resultado}`)
 }
-
